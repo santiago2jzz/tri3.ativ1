b821f34af6cd951f39c612598edb48afc07c7c26
Santiago <danielsantiagomendonca@gmail.com>
2026-09-18 16:47:45 -0300

    aula 18/09

--- index.ts
+++ index.ts
@@ -1,58 +1,70 @@
-//           Banco de Dados   HTTP
-// [C]reate  insert           post
-// [R]ead    select           get
-// [U]pdate  update           put
-// [U]pdate  update           patch
-// [D]elete  delete           delete
-// 
-
-import { db } from "./db"
+import { db } from "./db.ts";
 
 const srv = Bun.serve({
-    port: 3000,
-    routes: {
-        "/user": {
-                 GET: (req) => {
-                const query = db.query(`
-                    SELECT * FROM users
-                `)
-                const deResp = query.all ()
-                return Response.json(deResp)
-            },
-            
-            POST: async (req) => {
-                const body = await req.body.json();
-                const query = db.query(`
-                    INSERT INTO users(username, email, password_hash)
-                    VALUES(:username, :email, :password_hash)    
-                `)
-                const dbResp = query.run({
-                    ':username': body.username,
-                    ':email' : body.email,
-                    ':password_hash':  body.password
-                })
-                return Response.json({
-                    "message": "deu bom",
-                    dbResp
-                })
-            },
+  port: 3000,
+  routes: {
+    "/user": {
+      GET: (req) => {
+        const query = db.query(`
+          SELECT * FROM USERS
+          `);
+        const data = query.all();
+        return Response.json({ data });
+      },
+      POST: async (req) => {
+        const body = await req.body.json();
+        const query = db.query(`
+          INSERT INTO USERS (username, email, password_hash)
+          VALUES(:username, :email, :password_hash)
+          `);
+        const dbResp = query.run({
+          ":username": body.username,
+          ":email": body.email,
+          ":password_hash": body.password,
+        });
+        return Response.json({
+          message: "deu boa",
+          dbResp,
+        });
+      },
+    },
+    "/user/:id": {
+      GET: (req) => {
+        const id = req.params.id;
+        const query = db.query(`
+          SELECT * FROM users WHERE id = :id
+          `);
 
-        },
+        const data = query.get({
+          ":id": id
+        });
+        return Response.json({
+          data
+        });
+      },
 
-        "/user/:id": {
-            GET: (req) => {
-                const query = db.query(`
-                    SELECT * FROM users
-                    WHERE id=:_id_
-                `)
-                const deResp = query.get({ ":_id_": req.params.id })
-                return Response.json(deResp)
-            },
+      PUT: (req) => {
+        const id = req.params.id;
+        const body = req.body.json();
+        const query = db.query(`
+          UPDATE users SET username = :username, email = :email, password_hash = :password_hash WHERE id = :id
+          `);
+        const dbResp = query.run({
+          ":username": body.username,
+          ":email": body.email,
+          ":password_hash": body.password,
+          ":id": id
+        })
 
-            PUT: () => Response.json("", { status: 501 }),
-            DELETE: () => Response.json("", { status: 501 }),
-        }
-    }
-})
+        return Response.json({
+          message: "deu boa",
+          dbResp
+        });
+      },
+      
+      DELETE: () => Response.json("", { status: 501 }),
+    },
+  },
+});
 
-console.log(`Server running: ${srv.url}`)
+console.log(`servidor em ${srv.url}`);
